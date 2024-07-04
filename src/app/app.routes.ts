61e843e05b08b2e3af33e1ad56705022fe8a3826
Money <narenderjazwal@gmail.com>
2024-07-04 21:24:23 +0530

    init_commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './Admin/login/login.component';
+import { LayoutComponent } from './Admin/layout/layout.component';
+import { ProductsComponent } from './Admin/products/products.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  // these are just default routes of the application
+  {
+    path: '',
+    redirectTo: 'login',
+    pathMatch: 'full',
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
+  },
+  {
+    path: '',
+    component: LayoutComponent,
+    children: [
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+    ],
+  },
+];
